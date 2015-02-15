26f1fe2381a3caefc8c33bd4238699a406675497
Dimitrios Psarrou <dpsarrou@gmail.com>
2015-02-15 21:59:19 +0100

    wth did i just fixed

--- src/CminorFramework/UtilityBelt/Wordpress/Contracts/Attachment/IAttachmentHelper.php
+++ src/CminorFramework/UtilityBelt/Wordpress/Contracts/Attachment/IAttachmentHelper.php
@@ -14,19 +14,9 @@ interface IAttachmentHelper
 
     /**
      * Returns a decorated image instance associated with the provided $post
-     * @see \CminorFramework\UtilityBelt\Wordpress\Contracts\Image\IImageHelper::getDecoratedImage()
-     *
-     * Throws WordpressFunctionNotFoundException exception if $post is not instance of \WP_Post AND wordpress get_post method is not found to retrieve it
-     * Throws InvalidArgumentException exception if $post is not instance of \Wp_Post AND $post is not int
-     *
-     * @uses Wordpress method : get_post
-     * @uses Wordpress method : get_post_meta
-     * @throws \CminorFramework\UtilityBelt\Wordpress\Components\Exception\WordpressFunctionNotFoundException
-     * @throws \InvalidArgumentException
-     *
      * @param mixed:int|\WP_Post $post The post id OR the \Wp_Post object to be decorated
      * @param bool $fetch_meta_data if set to true, will also retrieve the post's metadata
-     * @return \CminorFramework\UtilityBelt\Wordpress\Contracts\Image\IDecoratedImage
+     * @return \CminorFramework\UtilityBelt\Wordpress\Contracts\Attachment\IDecoratedImage
      */
     public function getDecoratedImage($post, $fetch_meta_data = false);
 
@@ -35,7 +25,7 @@ interface IAttachmentHelper
 	 * @param \Wp_Post $post
 	 * @param array $meta_data_array
 	 * @param array $extra_data_array
-	 * @return \CminorFramework\UtilityBelt\Wordpress\Contracts\Post\IDecoratedPost
+	 * @return \CminorFramework\UtilityBelt\Wordpress\Contracts\Attachment\IDecoratedImage
 	 */
 	public function createDecoratedImage(\Wp_Post $post = null, array $meta_data_array = [], array $extra_data_array = []);
 
