5ba3cd2e70d8971e1d44d7adc85ace5ab52d9102
Dimitrios Psarrou <dpsarrou@gmail.com>
2015-02-15 19:15:25 +0100

    updated controller

--- src/CminorFramework/UtilityBelt/Wordpress/Components/Controllers/Controller.php
+++ src/CminorFramework/UtilityBelt/Wordpress/Components/Controllers/Controller.php
@@ -1,38 +1,14 @@
 <?php
-namespace CminorFramework\UtilityBelt\Wordpress\Extendables\Components\Controllers;
-use CminorFramework\UtilityBelt\Contracts\Wordpress\Extendables\Components\Controllers\Controller;
-use CminorFramework\UtilityBelt\Contracts\Wordpress\Extendables\Components\Views\View;
+namespace CminorFramework\UtilityBelt\Wordpress\Components\Controllers;
 
-class Controller implements Controller
+use CminorFramework\UtilityBelt\Wordpress\Contracts\Controllers\IController;
+
+abstract class Controller implements IController
 {
-
-    protected $_views = [];
-
-    /**
-     *
-     * @param string $view_name
-     * @return \CminorFramework\UtilityBelt\Contracts\Wordpress\Extendables\Components\Views\View
-     */
-    public function getView($view_name)
+    //@override
+    public function getViewData()
     {
 
-        if(isset($this->_views[$view_name])){
-            return $this->_views[$view_name];
-        }
-
-        return $this->_views[$view_name];
-    }
-
-    /**
-     * Sets the view to the controller
-     * @param View $view
-     * @return \CminorFramework\UtilityBelt\Wordpress\Extendables\Components\Controllers\Controller
-     */
-    public function setView(View $view)
-    {
-        $view_name = $view->getViewName();
-        $this->_views[$view_name] = $view;
-        return $this;
     }
 
 }
