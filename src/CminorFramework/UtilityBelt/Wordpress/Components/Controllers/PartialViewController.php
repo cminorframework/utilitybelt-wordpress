36d96e2fbd0714726cb15cdea449fa48221c137e
Dimitrios Psarrou <dpsarrou@gmail.com>
2015-02-16 09:37:23 +0100

    added partial view controller

---
+++ src/CminorFramework/UtilityBelt/Wordpress/Components/Controllers/PartialViewController.php
@@ -0,0 +1,15 @@
+<?php
+namespace CminorFramework\UtilityBelt\Components\Controllers;
+
+use CminorFramework\UtilityBelt\Wordpress\Components\Controllers\Controller;
+
+class PartialViewController extends Controller
+{
+
+    public function showPartialView($view_template_filename)
+    {
+
+    }
+
+
+}
